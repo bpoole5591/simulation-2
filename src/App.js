bf7b9f87d39714822c9837dd4e1edc4d98328ba3
Brian Poole <brianpooledev@gmail.com>
2017-12-22 14:35:21 -0600

    finished landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+// Modules
+import { Switch, BrowserRouter as Route } from "react-router-dom";
+
+// Components
+
+import Login from "./components/login/login";
+import Dashboard from "./components/dashboard/dashboard";
+
+// CSS
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path="./login" component={Login} />
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </div>
     );
   }
