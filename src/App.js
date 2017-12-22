27ee3ac47e503916a3cbbd08f36f9c5343ecc584
Beck TJ <bakytbek.tatibekov@gmail.com>
2017-12-22 15:34:22 -0600

    to be continued

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import WizardView from './src/components/wizard/step1/step1'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+       
+        <h1 className="App-title">Houser Dashboard</h1>
+      </header>
+
+      <div className="container">
+            
+            <div class="leftpane"><h1>1</h1></div>
+            <div class="middlepane">2</div>
+           
+
+
+
+</div>
+
       </div>
     );
   }
---
+++ src/components/wizard/step1/step1.js
@@ -0,0 +1,14 @@
+import React from ('react');
+import './styles.css'
+import { Link } from 'react-router-dom';
+
+const WizardView = () => {
+    <div>
+        
+        
+
+    </div>
+
+}
+
+export default WizardView;
