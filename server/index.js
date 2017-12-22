8baeb16c93f39af229466dcb5cfec87412162ded
Gabriella <gaba@Gabriellas-MacBook-Pro.local>
2017-12-22 11:40:05 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { json } = require ('body-parser');
+const axios = require('axios');
+const cors = require('cors');
+const massive = require ('massive');
+require('dotenv').config();
+const app = express();
+const port = 3001;
+
+app.use(cors());
+app.use(json());
+
+app.listen( port, () => {console.log(`server listening ${port} `); });
